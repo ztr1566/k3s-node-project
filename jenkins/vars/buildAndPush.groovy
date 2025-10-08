302b473ef14b7e86eb50243c475a214747ca3e10
Ziad <zizoo1566@gmail.com>
2025-10-08 14:52:57 +0000

    fix(jenkins): Correct shared library path and build context

--- jenkins/vars/buildAndPush.groovy
+++ jenkins/vars/buildAndPush.groovy
@@ -3,7 +3,7 @@ def call(String imageName, String dockerhubCredentialsId) {
         echo "Starting Docker build for ${imageName}..."
 
         docker.withRegistry('https://index.docker.io/v1/', dockerhubCredentialsId) {
-            def customImage = docker.build(imageName, '-f app/Dockerfile app')
+            def customImage = docker.build(imageName, 'app')
 
             echo "Pushing Docker image..."
             customImage.push()
