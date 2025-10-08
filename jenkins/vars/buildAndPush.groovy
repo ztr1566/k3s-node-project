1f9cf35db17599a27af03f98918a9846f601cc0c
Ziad <zizoo1566@gmail.com>
2025-10-08 14:47:06 +0000

    ADD: Jenkins pipeline.....

---
+++ jenkins/vars/buildAndPush.groovy
@@ -0,0 +1,14 @@
+def call(String imageName, String dockerhubCredentialsId) {
+    script {
+        echo "Starting Docker build for ${imageName}..."
+
+        docker.withRegistry('https://index.docker.io/v1/', dockerhubCredentialsId) {
+            def customImage = docker.build(imageName, '-f app/Dockerfile app')
+
+            echo "Pushing Docker image..."
+            customImage.push()
+        }
+
+        echo "Image ${imageName} pushed successfully!"
+    }
+}
