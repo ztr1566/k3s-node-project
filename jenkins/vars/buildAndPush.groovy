8dbc72bd651053e2f78467f2c6097ea349f742de
Ziad <zizoo1566@gmail.com>
2025-10-08 17:57:42 +0000

    fix(jenkins): Correct shared library path and build context

--- jenkins/vars/buildAndPush.groovy
+++ jenkins/vars/buildAndPush.groovy
@@ -1,16 +1,13 @@
+// ده ملف buildAndPush.groovy بتاعك
 def call(Map config) {
     def imageName = config.imageName
     def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
+    def context = config.context ?: '.' // Kaniko بيستخدم dir://app، لكن buildah بياخد المسار عادي
     def digestFileName = "image-digest.txt"
 
-    container('kaniko') {
-    sh '''
-        /kaniko/executor --context=dir://app \
-                         --dockerfile=app/Dockerfile \
-                         --destination=zizoo1566/my-node-app:${IMAGE_TAG} \
-                         --cleanup
-    '''
-}
+    container('buildah') {
+        sh "buildah bud --format docker -t ${imageName} -f ${dockerfile} ./app"
+        sh "buildah push ${imageName}"
+    }
     return digestFileName
 }
