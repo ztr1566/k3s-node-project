10ae086e9c942256d21b651194f21b227fc20954
Ziad <zizoo1566@gmail.com>
2025-10-08 17:51:31 +0000

    fix(jenkins): Correct shared library path and build context

--- jenkins/vars/buildAndPush.groovy
+++ jenkins/vars/buildAndPush.groovy
@@ -1,30 +1,16 @@
-// jenkins/vars/buildAndPush.groovy
 def call(Map config) {
     def imageName = config.imageName
-    def dockerfile = config.dockerfile ?: 'app/Dockerfile'
-    def context = config.context ?: 'app'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
     def digestFileName = "image-digest.txt"
 
-    container('buildah') {
-        // Buildah needs explicit login using credentials
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-
-            try {
-                echo "--- Logging in to Docker Hub with Buildah ---"
-                sh "buildah login -u '${DOCKER_USER}' -p '${DOCKER_PASS}' docker.io"
-
-                echo "--- Building image ${imageName} with Buildah ---"
-                sh "buildah bud -t ${imageName} -f ${dockerfile} ${context}"
-
-                echo "--- Pushing image ${imageName} with Buildah ---"
-                // The 'docker://' prefix is how Buildah knows to push to a Docker registry
-                sh "buildah push --digestfile ${digestFileName} ${imageName} docker://${imageName}"
-
-            } finally {
-                echo "--- Logging out from Docker Hub ---"
-                sh "buildah logout docker.io"
-            }
-        }
-    }
+    container('kaniko') {
+    sh '''
+        /kaniko/executor --context=dir://app \
+                         --dockerfile=app/Dockerfile \
+                         --destination=zizoo1566/my-node-app:${IMAGE_TAG} \
+                         --cleanup
+    '''
+}
     return digestFileName
 }
